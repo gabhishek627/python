518c031ac31856bd15ed92963b06256315ab35db
gabhishek627 <85788858+gabhishek627@users.noreply.github.com>
2026-02-20 12:01:38 +0530

    Update print.py

--- print.py
+++ print.py
@@ -68,6 +68,17 @@ The name of boy is,hari,sadu
 
 
 3. 
+The following example shows how the function returns a string even when the user introduces
+numbers:
+$ python3
+Python 3.9.10 (main, Feb 9 2022, 00:00:00)
+[GCC 11.2.1 20220127 (Red Hat 11.2.1-9)] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> response = input("Please enter a number: ")
+Please enter a number: 456
+>>> print("The response of", response, "is of type", type(response))
+The response of 456 is of type <class 'str'>
+>>> exit()
 
 
 
